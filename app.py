f102fc4e12a3bf0ca56de1f43f45c3ccb5ba1901
robodobo4444 <rowantevs@gmail.com>
2025-11-01 19:03:41 -0400

    Implement sign-up and submit functionality
    Added sign-up and submit routes for user registration.

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ def home():
     user_name = ""
     return render_template('index.html', name = user_name)
 
-    
-
 @app.route('/about')
 def about():
     return render_template('about.html')
@@ -20,6 +18,31 @@ def small_projects():
 @app.route('/major_projects')
 def major_projects():
     return render_template('major_projects.html')
+
+@app.route("/sign_up')
+def sign_up():
+    return render_template("Sign_up.html")
+@app.route("/submit", methods=["POST"])
+def submit():
+    username = request.form.get("username")
+    password = request.form.get("password")
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    with open(CSV_FILE, 'r') as f:
+        if username in file.read():
+            username_in_use = True
+        else:
+            username_in_use = False
+    if username_in_use = False
+    try:
+        with open(CSV_FILE, 'a', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow([timestamp, name, email, message])
+
+        flash('Data successfully saved!', 'success')
+    except Exception as e:
+        flash(f'Error saving data: {str(e)}', 'error')
+
+    return redirect("testing.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
